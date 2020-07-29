a4b401e2e7c47a895c047f7dabe25d16a6f0ca1c
pedroosorio98 <59745060+pedroosorio98@users.noreply.github.com>
2020-07-29 19:52:39 -0300

    Delete Função Retornos Anuais Yahoo Finance.py

--- Função Retornos Anuais Yahoo Finance.py
+++
@@ -1,7 +0,0 @@
-def retornos_anuais_yahoo(ticker): 
-    import yfinance as yf 
-    ativo_precos=yf.download(ticker)['Adj Close']
-    ativo_retornos = pd.DataFrame(ativo_precos.groupby([ativo_precos.index.year,ativo_precos.index.year]).tail(1).pct_change())
-    ativo_retornos = ativo_retornos.set_index(ativo_retornos.index.year)
-    ativo_retornos = ativo_retornos.rename(columns={'Adj Close':ticker})
-    return ativo_retornos
